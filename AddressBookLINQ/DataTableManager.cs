4fec03bf47d60b436a8689ee9626a2ba09cac3d3
sunilpowar <sunilpowarsp8468@gmail.com>
2022-03-15 20:46:25 +0530

    Adding UC1

---
+++ AddressBookLINQ/DataTableManager.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AddressBookLINQ
+{
+    public class DataTableManager
+    {
+        public static DataTable dataTable;
+        public void CreateDataTable()
+        {
+            dataTable = new DataTable("AddressBookSystemLinQ");
+            DataColumn dataColumn;
+            //Creating addressBookId column
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(int);
+            dataColumn.ColumnName = "ID";
+            dataColumn.Caption = "Address Book Id";
+            dataColumn.AutoIncrement = true;
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = true;
+            // Add column to the DataColumnCollection.  
+            dataTable.Columns.Add(dataColumn);
+
+            // Create First Name column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(String);
+            dataColumn.ColumnName = "FirstName";
+            dataColumn.Caption = "First Name";
+            dataColumn.AutoIncrement = false;
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            dataTable.Columns.Add(dataColumn);
+
+            // Create Last Name column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(String);
+            dataColumn.ColumnName = "LastName";
+            dataColumn.Caption = "Last Name";
+            dataColumn.AutoIncrement = false;
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            dataTable.Columns.Add(dataColumn);
+
+            // Create Address column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(String);
+            dataColumn.ColumnName = "Address";
+            dataColumn.Caption = "Address";
+            dataColumn.AutoIncrement = false;
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            dataTable.Columns.Add(dataColumn);
+
+            // Create city column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(String);
+            dataColumn.ColumnName = "City";
+            dataColumn.Caption = "City";
+            dataColumn.AutoIncrement = false;
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            dataTable.Columns.Add(dataColumn);
+
+            // Create State column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(String);
+            dataColumn.ColumnName = "State";
+            dataColumn.Caption = "State";
+            dataColumn.AutoIncrement = false;
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            /// Add column to the DataColumnCollection.  
+            dataTable.Columns.Add(dataColumn);
+
+            // Create Email column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(String);
+            dataColumn.ColumnName = "Email";
+            dataColumn.Caption = "Email";
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            dataTable.Columns.Add(dataColumn);
+
+            // Create PhoneNumber column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(Int64);
+            dataColumn.ColumnName = "PhoneNumber";
+            dataColumn.Caption = "PhoneNumber";
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            dataTable.Columns.Add(dataColumn);
+
+            // Create zip column.    
+            dataColumn = new DataColumn();
+            dataColumn.DataType = typeof(Int64);
+            dataColumn.ColumnName = "Zip";
+            dataColumn.Caption = "Zip";
+            dataColumn.ReadOnly = false;
+            dataColumn.Unique = false;
+            // Add column to the DataColumnCollection.    
+            dataTable.Columns.Add(dataColumn);
+
+            Console.WriteLine("Created Table Successfully!!!");
+        }
+              
+
+    }
+}
---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME to LINQ of AddressBook");
+            bool flag = true;
+            DataTableManager dataTableManager = new DataTableManager();
+            Console.WriteLine("Choose an option: ");
+            Console.WriteLine("1.Create Data Table\t2.exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    dataTableManager.CreateDataTable();
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
